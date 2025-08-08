d62bf11425b5f29cbeff8bba0e1e06794853343c
Jacky Liang <coolboii@me.com>
2025-08-08 11:51:31 -0400

    Fix postinstall issues
    ### FIXED
    - When manually installing package globally, `postinstall` script would run without `devDependencies` properly installed.

--- postinstall.js
+++ postinstall.js
@@ -1,6 +1,9 @@
 import { execSync } from 'node:child_process';
 
-if (process.env.npm_config_omit !== 'dev') {
+const isGlobal = process.env.npm_config_global === 'true';
+const isDevOmitted = process.env.npm_config_omit === 'dev';
+
+if (!isGlobal && !isDevOmitted) {
   console.info('Running post-install commands ...');
 
   execSync('npm run build', {
